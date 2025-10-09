d13fff133088e43f83a75801da51f92ff4ec802a
Alkiviadis Tzaras <alkiviadistzaras@yahoo.gr>
2025-10-09 15:05:41 +0200

    added tokens

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -6,9 +6,7 @@ from .search_utils import load_movies
 def search_titles(query: str) -> list[dict]:
     movies = load_movies()
     search_result = [
-        movie
-        for movie in movies
-        if preprocess_string(query) in preprocess_string(movie["title"])
+        movie for movie in movies if check_for_match(query, movie["title"])
     ]
     search_result_sorted = sorted(search_result, key=lambda x: x["id"])
     return search_result_sorted[:5]
@@ -22,3 +20,9 @@ def remove_punctuation(input_string: str) -> str:
 
 def preprocess_string(input_string: str) -> str:
     return remove_punctuation(input_string.lower())
+
+
+def check_for_match(query: str, movie_title: str) -> bool:
+    query_tokens = preprocess_string(query).split()
+    movie_title_tokens = preprocess_string(movie_title).split()
+    return any(item in movie_title_tokens for item in query_tokens)
