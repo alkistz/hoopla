4f30845f4ab2fc37eb2a1dcfad14f32d12550f9e
Alkiviadis Tzaras <alkiviadistzaras@yahoo.gr>
2025-10-09 22:45:33 +0200

    removed stop words

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,6 +1,6 @@
 import string
 
-from .search_utils import load_movies
+from .search_utils import load_movies, load_stop_wrods
 
 
 def search_titles(query: str) -> list[dict]:
@@ -22,7 +22,22 @@ def preprocess_string(input_string: str) -> str:
     return remove_punctuation(input_string.lower())
 
 
+def remove_stop_words(tokens: list[str]) -> list[str]:
+    stop_words = set(load_stop_wrods())
+    sanitised_tokens = set(tokens) - stop_words
+    return list(sanitised_tokens)
+
+
 def check_for_match(query: str, movie_title: str) -> bool:
     query_tokens = preprocess_string(query).split()
-    movie_title_tokens = preprocess_string(movie_title).split()
-    return any(item in movie_title_tokens for item in query_tokens)
+    query_tokens = remove_stop_words(query_tokens)
+
+    movie_tokens = preprocess_string(movie_title).split()
+    movie_tokens = remove_stop_words(movie_tokens)
+
+    for query_token in query_tokens:
+        for movie_token in movie_tokens:
+            if query_token in movie_token:
+                return True
+
+    return False
