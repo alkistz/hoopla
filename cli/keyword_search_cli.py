aa22b5eee1da49228d7ad16301bdc177ffa44a78
Alkiviadis Tzaras <alkiviadistzaras@yahoo.gr>
2025-10-29 15:56:51 +0100

    Refactor code structure for improved readability and maintainability

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -4,11 +4,14 @@ import argparse
 
 from lib.inverted_index import (
     InvertedIndex,
+    bm25_idf_command,
+    bm25_tf_command,
     idf_command,
     tf_command,
     tfidf_command,
     tokenise_text,
 )
+from lib.search_utils import BM25_B, BM25_K1
 
 
 def main() -> None:
@@ -34,6 +37,25 @@ def main() -> None:
     )
     tfidf_parser.add_argument("doc_id", help="Document ID")
     tfidf_parser.add_argument("term", help="Specific term")
+
+    bm25_idf_parser = subparsers.add_parser(
+        "bm25idf", help="Get BM25 IDF score for a given term"
+    )
+    bm25_idf_parser.add_argument(
+        "term", type=str, help="Term to get BM25 IDF score for"
+    )
+
+    bm25_tf_parser = subparsers.add_parser(
+        "bm25tf", help="Get BM25 TF score for a given document ID and term"
+    )
+    bm25_tf_parser.add_argument("doc_id", type=int, help="Document ID")
+    bm25_tf_parser.add_argument("term", type=str, help="Term to get BM25 TF score for")
+    bm25_tf_parser.add_argument(
+        "k1", type=float, nargs="?", default=BM25_K1, help="Tunable BM25 K1 parameter"
+    )
+    bm25_tf_parser.add_argument(
+        "b", type=float, nargs="?", default=BM25_B, help="Tunable BM25 b parameter"
+    )
 
     args = parser.parse_args()
 
@@ -74,6 +96,18 @@ def main() -> None:
             tfidf_score = tfidf_command(args.doc_id, args.term)
             print(
                 f"TF-IDF score of '{args.term}' in document '{args.doc_id}': {tfidf_score:.2f}"
+            )
+
+        case "bm25idf":
+            print("Calculating BM25 IDF")
+            bm25idf_score = bm25_idf_command(args.term)
+            print(f"BM25 IDF score of '{args.term}': {bm25idf_score:.2f}")
+
+        case "bm25tf":
+            print("Calculating the BM25 TF score")
+            bm25_tf_score = bm25_tf_command(args.doc_id, args.term, args.k1, args.b)
+            print(
+                f"BM25 TF score of '{args.term}' in document '{args.doc_id}': {bm25_tf_score:.2f}"
             )
 
         case _:
--- cli/lib/inverted_index.py
+++ cli/lib/inverted_index.py
@@ -6,7 +6,7 @@ from collections import Counter, defaultdict
 
 from nltk.stem import PorterStemmer
 
-from .search_utils import CACHE_DIR, load_movies, load_stop_wrods
+from .search_utils import BM25_B, BM25_K1, CACHE_DIR, load_movies, load_stop_wrods
 
 
 class InvertedIndex:
@@ -14,9 +14,11 @@ class InvertedIndex:
         self.index: dict[str, set] = defaultdict(set)
         self.docmap: dict[int, dict] = {}
         self.term_frequencies: dict = defaultdict(Counter)
+        self.doc_lengths = {}
         self.index_path = os.path.join(CACHE_DIR, "index.pkl")
         self.docmap_path = os.path.join(CACHE_DIR, "docmap.pkl")
         self.term_frequencies_path = os.path.join(CACHE_DIR, "term_frequencies.pkl")
+        self.doc_lengths_path = os.path.join(CACHE_DIR, "doc_lengths.pkl")
 
     def get_documents(self, term: str) -> list:
         """
@@ -27,38 +29,46 @@ class InvertedIndex:
             [self.docmap[doc_id] for doc_id in doc_ids], key=lambda x: x["id"]
         )
 
-    def get_tf(self, doc_id: str, term: str) -> int:
+    def get_tf(self, doc_id: int, term: str) -> int:
         """
         Return the times the token appears in the document with the given ID. If the term doesn't exist in that document, return 0.
         Be sure to tokenize the term, but assume that there is only one token. If there's more than one, raise an exception.
         """
-        tokenised_term = tokenise_text(term)
-        if len(tokenised_term) != 1:
-            raise ValueError("More than one tokenised term")
-
-        token = tokenised_term[0]
-
+        token = tokenise_term(term)
         if token not in self.term_frequencies[int(doc_id)]:
             return 0
 
         return self.term_frequencies[int(doc_id)].get(token, 0)
 
     def get_idf(self, term: str):
-        tokenised_term = tokenise_text(term)
-        if len(tokenised_term) != 1:
-            raise ValueError("More than one tokenised term")
-
-        token = tokenised_term[0]
+        token = tokenise_term(term)
         doc_count = len(self.docmap)
         term_doc_count = len(self.get_documents(token))
 
         return math.log((doc_count + 1) / (term_doc_count + 1))
 
-    def get_tfidf(self, doc_id: str, term: str):
-        token = tokenise_text(term)[0]
+    def get_tfidf(self, doc_id: int, term: str):
+        token = tokenise_term(term)
         idf_score = idf_command(token)
         tf_score = self.get_tf(doc_id, term)
         return tf_score * idf_score
+
+    def get_bm25_idf(self, term: str) -> float:
+        token = tokenise_term(term)
+        doc_count = len(self.docmap)
+        df = len(self.get_documents(token))
+        return math.log((doc_count - df + 0.5) / (df + 0.5) + 1)
+
+    def get_bm25_tf(
+        self, doc_id: int, term: str, k1: float = BM25_K1, b: float = BM25_B
+    ) -> float:
+        tf = self.get_tf(doc_id, term)
+
+        length_norm = (
+            1 - b + b * (self.doc_lengths[doc_id] / self.__get_avg_doc_length())
+        )
+
+        return (tf * (k1 + 1)) / (tf + k1 * length_norm)
 
     def build(self):
         """
@@ -79,6 +89,7 @@ class InvertedIndex:
         self.__write_to_file(self.index_path, self.index)
         self.__write_to_file(self.docmap_path, self.docmap)
         self.__write_to_file(self.term_frequencies_path, self.term_frequencies)
+        self.__write_to_file(self.doc_lengths_path, self.doc_lengths)
 
     def load(self):
         if not os.path.exists(self.index_path):
@@ -88,7 +99,10 @@ class InvertedIndex:
             raise FileExistsError("Docmap pickle does not exist")
 
         if not os.path.exists(self.term_frequencies_path):
-            raise FileExistsError("Docmap pickle does not exist")
+            raise FileExistsError("Term frequencies pickle does not exist")
+
+        if not os.path.exists(self.doc_lengths_path):
+            raise FileExistsError("Doc lengths pickle does not exist")
 
         with open(self.index_path, "rb") as file:
             self.index = pickle.load(file)
@@ -102,14 +116,25 @@ class InvertedIndex:
             self.term_frequencies = pickle.load(file)
             print("tf loaded")
 
+        with open(self.doc_lengths_path, "rb") as file:
+            self.doc_lengths = pickle.load(file)
+            print("tf loaded")
+
     def __add_document(self, doc_id: int, text: str):
         """
         Tokenize the input text, then add each token to the index with the document ID.
         """
         tokens = tokenise_text(text)
         self.term_frequencies[doc_id].update(tokens)
+        self.doc_lengths[doc_id] = len(tokens)
         for token in tokens:
             self.index[token].add(doc_id)
+
+    def __get_avg_doc_length(self) -> float:
+        if not self.doc_lengths:
+            return 0
+
+        return sum(self.doc_lengths.values()) / len(self.doc_lengths)
 
     def __write_to_file(self, pickle_file, data):
         with open(pickle_file, "wb") as file:
@@ -141,7 +166,15 @@ def tokenise_text(input_text: str) -> list[str]:
     return tokens
 
 
-def tf_command(doc_id: str, term: str):
+def tokenise_term(term: str) -> str:
+    tokenised_term = tokenise_text(term)
+    if len(tokenised_term) != 1:
+        raise ValueError("More than one tokenised term")
+
+    return tokenised_term[0]
+
+
+def tf_command(doc_id: int, term: str):
     index = InvertedIndex()
     index.load()
 
@@ -155,8 +188,24 @@ def idf_command(term: str):
     return index.get_idf(term)
 
 
-def tfidf_command(doc_id: str, term: str):
+def tfidf_command(doc_id: int, term: str):
     index = InvertedIndex()
     index.load()
 
     return index.get_tfidf(doc_id, term)
+
+
+def bm25_idf_command(term: str):
+    index = InvertedIndex()
+    index.load()
+
+    return index.get_bm25_idf(term)
+
+
+def bm25_tf_command(
+    doc_id: int, term: str, k1: float = BM25_K1, b: float = BM25_B
+) -> float:
+    index = InvertedIndex()
+    index.load()
+
+    return index.get_bm25_tf(doc_id, term, k1)
