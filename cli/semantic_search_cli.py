825190170a5033524daf0bd79ca27ac9dc01c04f
Alkiviadis Tzaras <alkiviadistzaras@yahoo.gr>
2025-10-30 15:44:57 +0100

    First setup of the model

--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -2,12 +2,23 @@
 
 import argparse
 
+from lib.semantic_search import verify_model
+
 
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    subparsers.add_parser(
+        "verify",
+        help="Verifies that the model is loaded and prints basic model info",
+    )
+
     args = parser.parse_args()
 
     match args.command:
+        case "verify":
+            verify_model()
         case _:
             parser.print_help()
 
