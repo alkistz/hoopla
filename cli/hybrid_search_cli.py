56297943b04832ed85c03d775d5f6f7f756a023b
Alkiviadis Tzaras <alkiviadistzaras@yahoo.gr>
2025-11-24 14:08:34 +0100

    Started hybrid search

--- cli/hybrid_search_cli.py
+++ cli/hybrid_search_cli.py
@@ -3,11 +3,49 @@ import argparse
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Hybrid Search CLI")
-    parser.add_subparsers(dest="command", help="Available commands")
+    subparser = parser.add_subparsers(dest="command", help="Available commands")
+
+    normalize_parser = subparser.add_parser(
+        "normalize", help="Normalise a list of numbers"
+    )
+    normalize_parser.add_argument(
+        "numbers", type=float, nargs="+", help="A list of numbers separated by space"
+    )
+
+    weigthed_search_parser = subparser.add_parser(
+        "weighted-search",
+        help="Performs a hybrid search combining keyword and semantic search",
+    )
+    weigthed_search_parser.add_argument("query", type=str, help="Search query")
+    weigthed_search_parser.add_argument(
+        "--alpha",
+        type=float,
+        default=0.5,
+        help="The weight between keyword and semantic scores",
+    )
+    weigthed_search_parser.add_argument(
+        "--limit", type=int, default=5, help="Search results limit"
+    )
 
     args = parser.parse_args()
 
     match args.command:
+        case "normalize":
+            numbers = args.numbers
+            if not numbers:
+                return
+
+            min_value = min(numbers)
+            max_value = max(numbers)
+
+            if min_value == max_value:
+                normalised_scores = [1 for val in numbers]
+            else:
+                normalised_scores = [
+                    (value - min_value) / (max_value - min_value) for value in numbers
+                ]
+            for score in normalised_scores:
+                print(f"* {score:.4f}")
         case _:
             parser.print_help()
 
--- cli/lib/hybrid_search.py
+++ cli/lib/hybrid_search.py
@@ -1,5 +1,7 @@
 import os
 
+from cli.lib.search_utils import ALPHA
+
 from .inverted_index import InvertedIndex
 from .semantic_search import ChunkedSemanticSearch
 
@@ -20,7 +22,34 @@ class HybridSearch:
         return self.idx.bm25_search(query, limit)
 
     def weighted_search(self, query, alpha, limit=5):
-        raise NotImplementedError("Weighted hybrid search is not implemented yet.")
+        bm25_results = self._bm25_search(query, limit * 500)
+        self.semantic_search.search_chunks(query, limit=limit * 500)
+        
+        scores = {}
 
     def rrf_search(self, query, k, limit=10):
         raise NotImplementedError("RRF hybrid search is not implemented yet.")
+
+
+
+            
+
+
+def hybrid_score(bm25_score, semantic_score, alpha=ALPHA):
+    return alpha * bm25_score + (1 - alpha) * semantic_score
+
+
+def normalise(numbers: list[float]) -> list[float] | list[int] | None:
+    if not numbers:
+        return
+
+    min_value = min(numbers)
+    max_value = max(numbers)
+
+    if min_value == max_value:
+        normalised_scores = [1 for val in numbers]
+    else:
+        normalised_scores = [
+            (value - min_value) / (max_value - min_value) for value in numbers
+        ]
+    return normalised_scores
